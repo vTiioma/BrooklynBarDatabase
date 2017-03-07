b3bb9a0fc7ed91ad4a9cd669544a25d97d3e779a
vTiioma <asilantyeva@yahoo.com>
2017-03-07 02:27:26 -0500

    Quick Update To Remove Dummy Response
    Changed dummy response to server side response.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/barlist', function (req,res) {
       db.query("SELECT * FROM brooklynBarconnection", function(error,rows,fields) {
         if (error === null) {
           console.log("Query successful.");
-          // res.json(rows);
+          res.json(rows);
         } else {
           console.log("Query failed.");
         }
@@ -31,26 +31,26 @@ app.get('/barlist', function (req,res) {
     db.release();
   })
 
-  bar1 = {
-    name: "First Bar",
-    number: "(111) 111-1111",
-    address: "123 Fake st."
-  };
-
-  bar2 = {
-    name: "Second Bar",
-    number: "(222) 222-2222",
-    address: "456 Fake st."
-  };
-
-  bar3 = {
-    name: "Third Bar",
-    number: "(333) 333-3333",
-    address: "789 Fake st."
-  };
-
-  var barlist = [bar1,bar2,bar3];
-  res.json(barlist);
+  // bar1 = {
+  //   name: "First Bar",
+  //   number: "(111) 111-1111",
+  //   address: "123 Fake st."
+  // };
+  //
+  // bar2 = {
+  //   name: "Second Bar",
+  //   number: "(222) 222-2222",
+  //   address: "456 Fake st."
+  // };
+  //
+  // bar3 = {
+  //   name: "Third Bar",
+  //   number: "(333) 333-3333",
+  //   address: "789 Fake st."
+  // };
+  //
+  // var barlist = [bar1,bar2,bar3];
+  // res.json(barlist);
 });
 
 app.listen(3000);
