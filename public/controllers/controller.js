f4b0e5ba1ab1794f0324b5235a7af310dd741731
vTiioma <asilantyeva@yahoo.com>
2017-03-09 01:21:19 -0500

    Added ability to add and remove bar info
    Added ability to add and remove bar info, updated table style, added
    table sorting.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,14 +2,88 @@ var barApp = angular.module("myApp",[]);
 
 barApp.controller('AppCtrl',['$scope','$http',
   function($scope,$http) {
-    console.log("Hello from controller.");
+    $scope.sortType = 'name';
+    $scope.reverseSort = false;
+    $scope.searchBar = "";
+    $scope.bar;
 
-    $http.get('/barlist').then(
-      function(response) {
-        console.log("I got the data I requested.");
-        console.log(response);
-        $scope.barlist = response.data;
-      }
-    );
+    $scope.sortData = function(id) {
+      $scope.reverseSort = ($scope.sortType === id) ? !$scope.reverseSort : false;
+      $scope.sortType = id;
+    }
+
+    var refresh = function() {
+      $http.get('/barlist').then(
+        function(response) {
+          console.log("I got the data I requested.");
+          console.log(response);
+          $scope.barlist = response.data;
+          $scope.bar = null;
+        }
+      );
+    }
+
+    $scope.addBarToDB = function() {
+      console.log($scope.bar);
+      $http.post('/barlist', $scope.bar).then(
+        function(response) {
+          console.log(response);
+          refresh();
+        }
+      );
+    }
+
+    $scope.remove = function(id) {
+      console.log(id);
+      $http.delete('/barlist/'+id).then(
+        function(response) {
+          console.log("test");
+          refresh();
+        }
+      );
+    }
+
+    refresh();
   }
 ]);
+
+barApp.filter('tel', function () {
+    return function (tel) {
+        if (!tel) { return ''; }
+
+        var value = tel.toString().trim().replace(/^\+/, '');
+
+        if (value.match(/[^0-9]/)) {
+            return tel;
+        }
+
+        var country, city, number;
+
+        switch (value.length) {
+            case 1:
+            case 2:
+            case 3:
+                city = value;
+                break;
+
+            default:
+                city = value.slice(0, 3);
+                number = value.slice(3);
+        }
+
+        if(number){
+            if(number.length>3){
+                number = number.slice(0, 3) + '-' + number.slice(3,7);
+            }
+            else{
+                number = number;
+            }
+
+            return ("(" + city + ") " + number).trim();
+        }
+        else{
+            return "(" + city;
+        }
+
+    };
+});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+var bodyParser = require("body-parser");
 
 var app = express();
 var connection = mysql.createPool({
@@ -7,17 +8,18 @@ var connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'brooklynBarconnection'
+  database: 'brooklynBarDB'
 });
 
 app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.json());
 
 app.get('/barlist', function (req,res) {
   console.log("I recieved a GET request.");
   connection.getConnection(function(error,db) {
     if (error === null) {
       console.log("Connected!");
-      db.query("SELECT * FROM brooklynBarconnection", function(error,rows,fields) {
+      db.query("SELECT * FROM barInfo", function(error,rows,fields) {
         if (error === null) {
           console.log("Query successful.");
           res.json(rows);
@@ -29,7 +31,7 @@ app.get('/barlist', function (req,res) {
       console.log("Error!");
     }
     db.release();
-  })
+  });
 
   // bar1 = {
   //   name: "First Bar",
@@ -51,6 +53,49 @@ app.get('/barlist', function (req,res) {
   //
   // var barlist = [bar1,bar2,bar3];
   // res.json(barlist);
+});
+
+app.post('/barlist', function (req,res) {
+  console.log(req.body);
+  connection.getConnection(function(error,db) {
+    if (error === null) {
+      console.log("Connected!");
+      db.query("INSERT INTO barInfo (name, number, address) VALUES (?,?,?)",
+      [req.body.name, req.body.number, req.body.address],
+      function(error,doc) {
+        if (error === null) {
+          console.log("Query successful.");
+          res.json(doc);
+        } else {
+          console.log("Query failed. " + error);
+        }
+      });
+    } else {
+      console.log("Error!");
+    }
+    db.release();
+  });
+});
+
+app.delete('/barlist/:id', function (req,res) {
+  var id = req.params.id;
+  //console.log(id);
+  connection.getConnection(function(error,db) {
+    if (error === null) {
+      console.log("Connected!");
+      db.query("DELETE FROM barInfo WHERE id=? LIMIT 1", [id], function(error,doc) {
+        if (error === null) {
+          console.log("Query successful.");
+          res.json(doc);
+        } else {
+          console.log("Query failed.");
+        }
+      });
+    } else {
+      console.log("Error!");
+    }
+    db.release();
+  });
 });
 
 app.listen(3000);
